181ad57a142aab7d06f551e526a640cc460baa3a
Mikalai Zarazaka <golmtz@gmail.com>
2023-03-15 22:42:58 +0300

    add prettier and eslint settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import { PATH } from './constant';
+import { Route, Routes } from 'react-router-dom'
+import { PATH } from './constant'
 
-import { Main, Layout, Page404, Loaders } from './pages';
+import { Main, Layout, Page404, Loaders } from './pages'
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path={'/'} element={<Layout />}>
-                <Route index element={<Main />} />
-                <Route path={PATH.LOADERS} element={<Loaders />} />
-                <Route path={'*'} element={<Page404 />} />
-            </Route>
-        </Routes>
-    );
-};
+	return (
+		<Routes>
+			<Route path={'/'} element={<Layout />}>
+				<Route index element={<Main />} />
+				<Route path={PATH.LOADERS} element={<Loaders />} />
+				<Route path={'*'} element={<Page404 />} />
+			</Route>
+		</Routes>
+	)
+}
 
-export default App;
+export default App

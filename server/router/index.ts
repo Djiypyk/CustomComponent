d4285b925af29b74c41cdaf2fdfc879ce56d7ece
Mikalai Zarazaka <golmtz@gmail.com>
2023-04-11 15:24:05 +0300

    fix reg validation

--- server/router/index.ts
+++ server/router/index.ts
@@ -7,14 +7,14 @@ const router = new Router()
 
 router.post(
 	'/registration',
-	// body('email').isEmail,
-	// body('password').isLength({ min: 4, max: 32 }),
+	body('email').isEmail(),
+	body('password').isLength({ min: 4, max: 32 }),
 	userController.registration,
 )
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activate)
 router.get('/refresh', userController.refresh)
-router.get('/users', authMiddlewares,  userController.getUsers, )
+router.get('/users', authMiddlewares, userController.getUsers)
 
 export default router

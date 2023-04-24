f92866902128409fd132be57171539e19375dc73
Mikalai Zarazaka <golmtz@gmail.com>
2023-04-24 16:33:22 +0300

    some upd eslint

--- src/pages/Navigation/UserPage/UserPage.tsx
+++ src/pages/Navigation/UserPage/UserPage.tsx
@@ -1,5 +1,24 @@
-export const UserPage = () => {
-	return <div>Hi</div>
-}
+import { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
+import { Navigate } from 'react-router-dom'
+
+import { Context } from '../../../main'
+import { ContainerBlock } from '../../../components'
+import { PATH } from '../../../constant'
+
+export const UserPage = observer(() => {
+	const { store } = useContext(Context)
+
+	if (!store.isAuth) {
+		return <Navigate to={PATH.MAIN} />
+	}
+	return (
+		<div>
+			<ContainerBlock>
+				<h1>Hi</h1>
+			</ContainerBlock>
+		</div>
+	)
+})
 
 export default UserPage

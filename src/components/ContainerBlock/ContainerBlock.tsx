938cb354883d7b3e9e1967d26167f2c8dae7dafc
Mikalai Zarazaka <golmtz@gmail.com>
2023-04-28 14:54:40 +0300

    add gsap animation

--- src/components/ContainerBlock/ContainerBlock.tsx
+++ src/components/ContainerBlock/ContainerBlock.tsx
@@ -1,13 +1,22 @@
-import { FC, ReactNode } from 'react'
+import { FC, MutableRefObject, ReactNode } from 'react'
 
 import styles from './ContainerBlock.module.css'
 
 interface ContainerBlockProps {
 	children: ReactNode
 	classNameProps?: string
+	forwardedRef?: MutableRefObject<HTMLDivElement | null>
 }
 
-export const ContainerBlock: FC<ContainerBlockProps> = ({ children, classNameProps }) => {
-	return <div className={`${styles.wrapper} ${classNameProps}`}>{children}</div>
+export const ContainerBlock: FC<ContainerBlockProps> = ({
+	children,
+	classNameProps,
+	forwardedRef,
+}) => {
+	return (
+		<div ref={forwardedRef} className={`${styles.wrapper} ${classNameProps}`}>
+			{children}
+		</div>
+	)
 }
 export default ContainerBlock
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,36 @@
 import { observer } from 'mobx-react-lite'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 
-import { ContainerBlock } from '../../components'
 import styles from './Home.module.css'
 
+import { ContainerBlock } from '../../components'
+
 export const Home = () => {
+	const containerBlockRef = useRef(null)
+
+	useEffect(() => {
+		gsap.fromTo(
+			containerBlockRef.current,
+			{
+				duration: 1,
+				opacity: 0,
+				y: -40,
+			},
+			{ opacity: 1, y: 0 },
+		)
+	}, [])
+
 	return (
 		<div className={styles.container}>
-			<ContainerBlock>
+			<ContainerBlock forwardedRef={containerBlockRef}>
 				<span className={styles.title}>
-					This site is for you. <br /> I try to find interesting customization options for <br />
+					This site is for you. <br /> I try to find interesting customization
+					options for <br />
 					different components and provide them to you.
 				</span>
 			</ContainerBlock>
 		</div>
 	)
 }
-
 export default observer(Home)
